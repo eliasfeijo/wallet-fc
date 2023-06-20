2c586aee5c4993f940f30518ea14ab443873d847
C1320805 Elias Feijo de Almeida Pereira Lima <c1320805@interno.bb.com.br>
2023-06-20 04:18:53 -0300

    feat: docker compose mysql

--- cmd/wallet_consumer/main.go
+++ cmd/wallet_consumer/main.go
@@ -11,13 +11,16 @@ import (
 )
 
 func main() {
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", "root", "root", "mysql", "3307", "wallet_consumer"))
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", "root", "root", "localhost", "3307", "wallet_consumer"))
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 	db.Exec("CREATE TABLE IF NOT EXISTS account_balances (account_id VARCHAR(255) PRIMARY KEY, balance INT, date_time DATETIME)")
-	seedDB(db)
+	rows, _ := db.Query("SELECT 1 FROM account_balances LIMIT 1")
+	if !rows.Next() {
+		seedDB(db)
+	}
 }
 
 func seedDB(db *sql.DB) {
