26383cad275fab593a9dd14ab004d9b456dece83
C1320805 Elias Feijo de Almeida Pereira Lima <c1320805@interno.bb.com.br>
2023-06-20 04:03:21 -0300

    feat: main.go db config and seed

---
+++ cmd/wallet_consumer/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/eliasfeijo/wallet-consumer-golang/database/model"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+)
+
+func main() {
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", "root", "root", "mysql", "3307", "wallet_consumer"))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	db.Exec("CREATE TABLE IF NOT EXISTS account_balances (account_id VARCHAR(255) PRIMARY KEY, balance INT, date_time DATETIME)")
+	seedDB(db)
+}
+
+func seedDB(db *sql.DB) {
+	ab := model.NewAccountBalance(uuid.New().String(), 0, time.Now())
+	err := insertAccountBalance(db, ab)
+	if err != nil {
+		panic(err)
+	}
+	ab = model.NewAccountBalance(uuid.New().String(), 1000, time.Now())
+	err = insertAccountBalance(db, ab)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func insertAccountBalance(db *sql.DB, ab *model.AccountBalance) error {
+	stmt, err := db.Prepare("INSERT INTO account_balances (account_id, balance, date_time) VALUES (?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(ab.AccountID, ab.Balance, ab.DateTime)
+	if err != nil {
+		return err
+	}
+	return nil
+}
